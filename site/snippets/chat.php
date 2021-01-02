b8cb17231a17eb680659d29cf91846beb2bbe82e
Max Kohler <max@koehler-kn.de>
2021-01-02 12:00:15 +0000

    Embed chat iframe directly, skip initialisation script

--- site/snippets/chat.php
+++ site/snippets/chat.php
@@ -1,2 +1,3 @@
 <div class="chat-widget">
+<iframe style="width: 100%; height: 100%" src="//embed.tlk.io/export-live?theme=theme--night" frameborder="0"></iframe>
 </div>
--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -2,7 +2,6 @@
 <?= js('assets/js/resize.js') ?>
 <?= js('assets/js/podcast.js') ?>
 <?= js('assets/js/mess.js') ?>
-<?= js('assets/js/chat.js') ?>
 
 </body>
 </html>
