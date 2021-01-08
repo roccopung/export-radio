d9e42414e7d12144996d41b31e8d1b0be534af8b
roccopung <rocco.punghellini@gmail.com>
2021-01-08 14:29:45 +0000

    Tags page_mobile

--- site/templates/archive.php
+++ site/templates/archive.php
@@ -1,6 +1,7 @@
 <?php snippet('header-mobile') ?>
 
 		<div id="container-mobile">
+			<div class="button sticky" name="tags"> <a href="<?= $pages->find('tags')->url() ?>">Tags</a> </div>
 
 			<div id="wrap-mobile-archive">
 				<?php snippet('archive') ?>
